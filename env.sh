b8a1863358f184be23fdc687e87383b97e6e49b1
David Parra Perez <dparra0007@users.noreply.github.com>
2017-07-19 18:09:30 +0200

    update to variables of servicebus

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export ENV TODOAPI_MESSAGEBROKER_HOSTNAME="todoapi-messagebroker"
-export ENV TODOAPI_MESSAGEBROKER_USERNAME="admin"
-export ENV TODOAPI_MESSAGEBROKER_PASSWORD="mypass"
+export ENV TODOAPI_MESSAGEBROKER_RESOURCEURI="https://todoapi-messagebroker-namespace.servicebus.windows.net/todoapi-messagebroker-topic"
+export ENV TODOAPI_MESSAGEBROKER_KEYNAME="RootManageSharedAccessKey"
+export ENV TODOAPI_MESSAGEBROKER_KEY="7y/vs9oUg93rhGCSNuAQ5opUtfFW72OSfzC8cuaGGTA="
